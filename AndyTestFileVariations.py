bdec5c23c395a581e92a225edea98c2d98f2ab85
David Brewster <brewc@localhost.localdomain>
2016-10-12 23:48:20 -0400

    modified sample.py and put minimal comments

--- AndyTestFileVariations.py
+++ AndyTestFileVariations.py
@@ -166,22 +166,29 @@ def getPartsTest():
 
     print totalAverages.index(max(totalAverages))
 
-getPartsTest()
+# getPartsTest()
 #getPartsInfo('SummerNo3.mid')
 
 def find_theme(notes):
     """
     David's algorithim for identifying the theme in a given list of notes
     """
-    # print len(notes), notes
+
+    # prints the matrix representation of the lookup table
     def print_grid(grid):
         print numpy.matrix(grid)
+    
+    # safely returns the data of the given index
     def get(r, c, grid):
         if 0 <= r < len(grid) and 0 <= c < len(grid[0]):
             return grid[r][c]
         else:
             return 0
+
+    # stores the intermediate results of the caculations to avoid exponential runtime
     lookup_table = [[0 for j in xrange(len(notes))] for i in xrange(len(notes))]
+
+    # calculate all possible repeating subsequences
     for row in xrange(len(lookup_table)):
         for column in xrange(len(lookup_table[row])):
             max_of_neighbors = max(
@@ -194,9 +201,10 @@ def find_theme(notes):
                 lookup_table[row][column] = max_of_neighbors
     theme_notes = []
     current = 0
+
+    # backtrack to answer
     for index in xrange(len(notes)):
         if lookup_table[~0][index] > current:
             theme_notes.append(notes[index])
             current += 1
-    # print_grid(lookup_table)
     return theme_notes
--- sample.py
+++ sample.py
@@ -1,5 +1,6 @@
 import music21 as music
 from algorithms import *
+from AndyTestFileVariations import getPartsInfo
 import os
 
 SONG = 'fur_elise'
@@ -7,9 +8,9 @@ FOLDER = 'songs'
 DIRECTORY = '{0}/{1}'.format(FOLDER, SONG)
 FILE = '{0}.mid'.format(DIRECTORY)
 
-if not os.path.exists(DIRECTORY):
-    print 'made', DIRECTORY
-    os.makedirs(DIRECTORY)
+# if not os.path.exists(DIRECTORY):
+    # print 'made', DIRECTORY
+    # os.makedirs(DIRECTORY)
 
 
 def get_notes(file_name):
@@ -20,6 +21,7 @@ def get_notes(file_name):
             pitches = []
             note_durations = []
             for note in part.flat.notes:
+                print note.xPosition
                 if type(note) == music.chord.Chord:
                     pitches.append(note.pitchNames)
                 elif type(note) == music.note.Note:
@@ -32,21 +34,25 @@ def get_notes(file_name):
     except:
         'File not found.'
 
+def get_theme():
+    parts = get_notes(FILE)
 
-parts = get_notes(FILE)
+    for index, (part, (notes, note_durations)) in enumerate(parts.items()):
+        print 'processing part {0} ...'.format(index)
+        stream = music.stream.Stream()
+        theme = find_theme(zip(notes, note_durations))
+        for noise in theme:
+            if type(noise[0]) == list:
+                note = music.chord.Chord(noise[0])
+            else:
+                # print noise
+                note = music.note.Note(noise[0])
+            note.duration = noise[1]
+            stream.append(note)
+        if len(stream.pitches) != 0:
+            pass
+            # stream.write('midi', fp='{0}/{1}{2}.mid'.format(DIRECTORY, SONG, index))
+        # print map(lambda note: (note[0], note[1].type), theme)
 
-for index, (part, (notes, note_durations)) in enumerate(parts.items()):
-    print 'processing part {0} ...'.format(index)
-    stream = music.stream.Stream()
-    theme = find_theme(zip(notes, note_durations))
-    for noise in theme:
-        if type(noise[0]) == list:
-            note = music.chord.Chord(noise[0])
-        else:
-            # print noise
-            note = music.note.Note(noise[0])
-        note.duration = noise[1]
-        stream.append(note)
-    if len(stream.pitches) != 0:
-        stream.write('midi', fp='{0}/{1}{2}.mid'.format(DIRECTORY, SONG, index))
-    # print map(lambda note: (note[0], note[1].type), theme)
+# song = corpus.parse(FILE)
+# song.plot('pianoroll')
