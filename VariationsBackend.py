a4a13fab053c4452b46ec680446270988adf72d3
Andy <andylee4632@gmail.com>
2016-10-22 15:58:04 -0500

    Updated port

--- VariationsBackend.py
+++ VariationsBackend.py
@@ -31,4 +31,4 @@ def uploaded_file(filename):
     return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0')
+    app.run(host = '0.0.0.0', port=80)
