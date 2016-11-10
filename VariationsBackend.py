03e875edad9dd1cd127965dd6e4d33cdc3aa37ab
Andy <andylee4632@gmail.com>
2016-11-10 17:14:57 -0600

    Added API to Backend code

--- VariationsBackend.py
+++ VariationsBackend.py
@@ -8,7 +8,11 @@ app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = os.path.abspath('uploads/')+"/"
 app.config['DOWNLOAD_FOLDER'] = os.path.abspath('toDownload/')+"/"
-app.config['ALLOWED_EXTENSIONS'] = set(['mid'])
+app.config['ALLOWED_EXTENSIONS'] = set(['mid','midi','xml'])
+
+#Directories for API
+app.config['API_UPLOADS'] = os.path.abspath('APIuploads/')+"/"
+app.config['API_DOWNLOAD'] = os.path.abspath('APItoDownload')+"/"
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -20,18 +24,23 @@ def index():
 
 @app.route('/uploads', methods=['POST'])
 def upload():
-    file = request.files['file'] #Type <class 'werkzeug.datastructures.FileStorage'>
-    if file and allowed_file(file.filename):
-        curTime = str(time())
-        filename = secure_filename(file.filename) #filename is type str
-        filename = filename[:len(filename)-4] + curTime + '.mid'
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        try:
-            randomizedSong = simpleFileRandomizer(app.config['UPLOAD_FOLDER'] + filename)
-        except TypeError:
-            return redirect(url_for('variate_error'))
-        fp = randomizedSong.write('midi', fp=app.config['DOWNLOAD_FOLDER'] + filename)
-        return redirect(url_for('uploaded_file', filename=filename))
+    uploaded_files = request.files.getlist('file[]') #creates list of FileStorage objects
+    filenames = []
+    for file in uploaded_files:
+        if file and allowed_file(file.filename):
+            curTime = str(time())
+            filename = secure_filename(file.filename) #filename is type str
+            filename = filename[:len(filename)-4] + curTime + '.mid'
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            try:
+                randomizedSong = simpleFileRandomizer(app.config['UPLOAD_FOLDER'] + filename)
+            except TypeError:
+                return redirect(url_for('variate_error'))
+            fp = randomizedSong.write('midi', fp=app.config['DOWNLOAD_FOLDER'] + filename)
+            filenames.append(filename)
+    for file in filenames:
+        redirect(url_for('uploaded_file', filename=file))
+    return redirect(url_for('index'))
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
@@ -39,7 +48,31 @@ def uploaded_file(filename):
 
 @app.route('/error/')
 def variate_error():
-    return "Cannot process this file, please choose another"
+    return "Cannot process file(s), please try different file(s)"
+
+
+@app.route('/api/variate', methods=['POST'])
+def uploadAPI():
+    file = request.files['files']  # Type <class 'werkzeug.datastructures.FileStorage'>
+    if file and allowed_file(file.filename):
+        curTime = str(time())
+        filename = secure_filename(file.filename)  # filename is type str
+        filename = filename[:len(filename) - 4] + curTime + '.mid'
+        file.save(os.path.join(app.config['API_UPLOADS'], filename))
+        try:
+            randomizedSong = simpleFileRandomizer(app.config['API_UPLOADS'] + filename)
+        except TypeError:
+            return redirect(url_for('api_error'))
+        fp = randomizedSong.write('midi', fp=app.config['API_DOWNLOAD'] + filename)
+        return redirect(url_for('api_uploaded_file', filename=filename))
+
+@app.route('/api/uploads/<filename>')
+def api_uploaded_file(filename):
+    return send_from_directory(app.config['API_DOWNLOAD'], filename)
+
+@app.route('/api/error/')
+def api_error():
+    return "Cannot process file(s), please try different file(s)"
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port=80)
