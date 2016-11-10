b02c359198d6467bb2e859aaec5ab59bfa4de7d3
Charles Young <542charlieyoung@gmail.com>
2016-11-09 22:13:48 -0600

    regression

--- regression.py
+++ regression.py
@@ -8,9 +8,9 @@ timestamp = time.time()
 for i in xrange(len(corpus.getBachChorales())):
     regression_stream = combined.simpleFileRandomizer(corpus.getBachChorales()[i])
 
-    if not os.path.exists("regression\\" + str(i)):
-        os.makedirs("regression\\" + str(i))
+    if not os.path.exists("regression/" + str(i)):
+        os.makedirs("regression/" + str(i))
 
-    regression_stream.write("midi", fp="regression\\" + str(i) + "\\" + str(timestamp) + ".mid")
+    regression_stream.write("midi", fp="regression/" + str(i) + "/" + str(timestamp) + ".mid")
 
 print "Finished in " + str(time.time() - timestamp) + " seconds."
